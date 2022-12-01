a2b598e7c979a503e08e7a00b16b2c1d5119faa9
labestia2080 <100966021+reymichel2009@users.noreply.github.com>
2022-12-01 17:28:47 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,3 @@
-api_hash = "e3ce8145aa657c2a4cc5cf0f7183e476"
-api_id = 13233271
-bot_token = "5468195080:AAGcb6RCNJParhJ2Vt-OtFp3PG5nETQMCJo"
-bot_admin_group = -1001505671370
+api_hash = "d1a5974070430293b64fde5339591447"
+api_id = 17158127
+bot_token = "5280402950:AAGRPrZNSsjXpsfnfYs4WuVCgMCZWjzxWEg"
